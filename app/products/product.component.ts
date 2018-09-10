292b4462323c2696674fe665eee60e825428eb22
Adrian Bright <adman020@gmail.com>
2018-09-10 12:28:41 +0100

    19:26

--- app/products/product.component.ts
+++ app/products/product.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from './product.service';
+import {Product} from './product';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'home.template.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+    products:Product[];
+
+    constructor(private _productService: ProductService) {}
+
+    ngOninit() {
+        this.getProducts();
+    }
+
+    getProducts() {
+        this.products = this._productService.getProductsFromData();
+    }
 
 }
--- app/products/product.service.ts
+++ app/products/product.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ProductService } from './product';
+import { Product } from './product';
+import { PRODUCT_ITEMS } from './product-data';
 
 
 @Injectable() 
 export class ProductService {
-    
+    private pitems = PRODUCT_ITEMS;
+
+
+    getProductsFromData(): Product[] {
+        console.log(this.pItems);
+        return this.pItems;
+    }
+
+    /*getProductsFromService(): Product[] {
+    return [{
+        id:1,
+        name: 'Scissors',
+        description: 'Use this to cust stuff',
+        price: 4.99
+    },{
+    id:2,
+        name: 'Steak Knives',
+        description: 'Use this to eat food with',
+        price: 10.99
+    },{
+    id:3,
+        name: 'Shot Glasses',
+        description: 'Use this to take shots',
+        price: 5.99
+    }]
+    }*/
 }
